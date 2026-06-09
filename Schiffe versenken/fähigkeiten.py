04ba42cc906d778383e367aefcbc76fff86bc5ca
BenKlotz <mm2k5rd47m@privaterelay.appleid.com>
2026-06-09 10:16:45 +0200

    Update Fähigkeiten

--- Schiffe versenken/fähigkeiten.py
+++ Schiffe versenken/fähigkeiten.py
@@ -1,25 +1,34 @@
 #Fahigkeitspunkte:
 # Pro Runde: 1 Punkt
 # Pro eigenes Schiff getroffen: 1 Punkt
+# LED gibt an wieviele Punkte man grade hat in Farbe Grün!!!!
+
+# Fähigkeitstaste wird gededrückt:
+#   - Danach kann eine Fähikeit ausgewählt werden (FALLS GENUG PUNKTE)          -->     mit Y kann die Fähigkeitenauswahl beendet werden
+#   - mit der Taste A kann die kann die Fähikeit ausgewählt werden              -->     mit Y kann die Fähigkeitenauswahl beendet werden
+#   - danach kann sie durch die Bewegungstaste den Ort festgelegt werden        -->     mit Y kann die Fähigkeitenauswahl beendet werden
+#   - Folgend kann mit der Taste A die Fähikeit ausgeführt werden               -->     mit Y kann die Fähigkeitenauswahl beendet werden
 
 
-#Fähigkeit: Mine            Kosten: 2 Puntke 
+
+
+# Fähigkeit: Mine            Kosten: 2 Puntke 
 #Legt eine Mine in eigenes Spielfeld. 
 #Wenn Mine getroffen:
 #           Vibrationsmotor 2 sec und LED ROT 
 #           hat man 2 Random- Schüsse beim Gegnerischen Spielfeld 
 
 
-#Fähigekeit: Scannen        Kosten: 3 Punkte
+# Fähigekeit: Scannen        Kosten: 3 Punkte
 #Scannt 3x3 Feld beim Gegner und gibt an wieviel Objekte(Schiffe & Minen) sich in dem Berreich befinden 
 #Gibts aus z.B 3 Objekte 
-
 # Ziegt an der LED an wieviele objekte sich in dem Berreich befinden 
 
 
-#Fähigkeit: Air- Strike 5 Punkte
+# Fähigkeit: Air- Strike 5 Punkte
 # Zerstört ganze Spalte oder Zeile 
-# kann rotiert werden
+# Drehung nach Azswahl möglich 
+# 
 # 
 
 
