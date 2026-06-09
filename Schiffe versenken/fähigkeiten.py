53dd8b8a30b47cd5fda335110d0f3ff5144771ad
BenKlotz <mm2k5rd47m@privaterelay.appleid.com>
2026-06-09 10:05:25 +0200

    Beschreibung

---
+++ Schiffe versenken/fähigkeiten.py
@@ -0,0 +1,25 @@
+#Fahigkeitspunkte:
+# Pro Runde: 1 Punkt
+# Pro eigenes Schiff getroffen: 1 Punkt
+
+
+#Fähigkeit: Mine            Kosten: 2 Puntke 
+#Legt eine Mine in eigenes Spielfeld. 
+#Wenn Mine getroffen:
+#           Vibrationsmotor 2 sec und LED ROT 
+#           hat man 2 Random- Schüsse beim Gegnerischen Spielfeld 
+
+
+#Fähigekeit: Scannen        Kosten: 3 Punkte
+#Scannt 3x3 Feld beim Gegner und gibt an wieviel Objekte(Schiffe & Minen) sich in dem Berreich befinden 
+#Gibts aus z.B 3 Objekte 
+
+# Ziegt an der LED an wieviele objekte sich in dem Berreich befinden 
+
+
+#Fähigkeit: Air- Strike 5 Punkte
+# Zerstört ganze Spalte oder Zeile 
+# kann rotiert werden
+# 
+
+
